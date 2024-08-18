aa9a470f93f7bcbbf20c00456d2f9c15103b761b
monspear <tlcks1211@naver.com>
2024-08-18 21:22:40 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          성공적인 것인가? <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main-nav">
+        <p>연습용 실시간 채팅</p>
+      </div>
+      <div className="main-body">
+        <div className="main-body-contents">
+          <a href="/"><p>로그인</p></a>
+          <a href="/"><p>회원가입</p></a>
+        </div>
+      </div>
+      <div className="main-footer">
+        <h4>이 프로젝트는 원작자가 존재하며, 사용시 원작자의 허락이 요구됩니다.</h4>
+        <h4>원작자의 동의를 구하고 싶으면 tlcks1211@gmail.com에 메일로 동의를 구하시고, 답변을 얻으시길 바랍니다.</h4>
+      </div>
     </div>
   );
 }
