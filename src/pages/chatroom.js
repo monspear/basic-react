71bb8d0120c86a23520d814faeb7b458206ceb38
monspear <tlcks1211@naver.com>
2024-09-25 18:24:36 +0900

    update

--- src/pages/chatroom.js
+++ src/pages/chatroom.js
@@ -13,24 +13,52 @@ function Chatroom(){
                 <div className="my-chat">
                     <div className="my-chat-wrap">
                         <div className="chat-info">
-                            <div className="time-zone">2024-09-19 19:00</div>
-                            <div className="name"> 이름1</div>
+                            <div className="time-zone"> 2024-09-19 19:00 &nbsp; </div>
+                            <div className="name"> | 이름1 &nbsp;</div>
                             <div className="my-chat-img"><img src="/" ></img></div>
                         </div>
                         <div className="my-chat-content">안녕하세요.</div>
                     </div>
                 </div>
                 <div className="the-other-chat">
-                    <p>안녕하세요. 오늘 점심은 먹었어요?</p>
+                    <div className="the-other-chat-wrap">
+                        <div className="chat-info">
+                            <div className="chat-img"><img src="/" ></img></div>
+                            <div className="name"> &nbsp; 이름2 | </div>
+                            <div className="time-zone"> &nbsp; 2024-09-19 19:00 </div>
+                        </div>
+                        <div className="the-other-chat-content">안녕하세요. 오늘 점심은 먹었어요?</div>
+                    </div>
                 </div>
                 <div className="my-chat">
-                    <p>네 저는 라면 먹었어요.</p>
+                <div className="my-chat-wrap">
+                        <div className="chat-info">
+                            <div className="time-zone"> 2024-09-19 19:00 &nbsp; </div>
+                            <div className="name"> | 이름1 &nbsp;</div>
+                            <div className="my-chat-img"><img src="/" ></img></div>
+                        </div>
+                        <div className="my-chat-content">네 저는 라면 먹었습니다.</div>
+                    </div>
                 </div>
                 <div className="the-other-chat">
-                    <p>저는 갈비탕먹었습니다.</p>
+                    <div className="the-other-chat-wrap">
+                        <div className="chat-info">
+                            <div className="chat-img"><img src="/" ></img></div>
+                            <div className="name"> &nbsp; 이름2 | </div>
+                            <div className="time-zone"> &nbsp; 2024-09-19 19:00 </div>
+                        </div>
+                        <div className="the-other-chat-content">저는 갈비탕 먹었습니다.</div>
+                    </div>
                 </div>
                 <div className="the-other-chat">
-                    <p>전 도시락 먹었네요.</p>
+                    <div className="the-other-chat-wrap">
+                        <div className="chat-info">
+                            <div className="chat-img"><img src="/" ></img></div>
+                            <div className="name"> &nbsp; 이름2 | </div>
+                            <div className="time-zone"> &nbsp; 2024-09-19 19:00 </div>
+                        </div>
+                        <div className="the-other-chat-content">저는 도시락 먹었네요</div>
+                    </div>
                 </div>
             </div>
             <div className="chat-write">
